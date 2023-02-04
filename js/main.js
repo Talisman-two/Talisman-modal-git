c7ca1e8e1e583a28307dc7df0f9419702655f647
Eldor14 <eldorisroilov990@gmail.com>
2023-02-04 17:35:44 +0500

    To-do-madal new update

--- js/main.js
+++ js/main.js
@@ -6,24 +6,30 @@ $(document).ready(function () {
     $(".btn_one").click(function(e){
         cb("1% , -300px");
         $(".span_phone").html(samsung);
+        console.log(samsung);
     });
+    $(".btn").click(function(e){
+        $(".overlay").show()
+    })
     $(".btn_two").click(function(e){
         cb("1% , -300px");
         $(".span_phone").html(iphone);
+        console.log(iphone);
     });
     $(".btn_tree").click(function(e){
         cb("1% , -300px");
         $(".span_phone").html(xiaomi);
+        console.log(xiaomi);
     });
     function cb(){
         popup.style.transform = "translate(1% ,-300px)"
     };
     $(".close-btn").click(function(e){
-
         if(popup.style.transform == "translate(1% ,-300px)"){
             popup.style.transform = "translate(1% , -300px)"
         }else{
             popup.style.transform = "translate(-1110% , 200%)"
+        $(".overlay").hide()
         }
     })
 });
